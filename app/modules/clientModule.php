83dff088bcbbaab3ab8c96818ab11d1b1893711b
salmaniz82 <salmaniz.82@gmail.com>
2018-08-11 04:37:11 +0500

    Modular structure added for testing

---
+++ app/modules/clientModule.php
@@ -0,0 +1,30 @@
+<?php
+class clientModule {
+
+
+	public function isClientLinkedToVendor($clientID, $vendorID)
+	{
+
+
+	}
+
+	public function addCleintToList($clientID, $vendorID)
+	{
+
+	}
+
+	public function removeClientFromVendor($clientID, $vendorID)
+	{
+
+	}
+
+
+	public function removeVendorFromClient($clientID, $vendorID)
+	{
+
+		
+	}
+
+
+	
+}
---
+++ app/modules/invoiceModule.php
@@ -0,0 +1,28 @@
+<?php
+class invoiceModule {
+
+
+	public function listVendorInvoice()
+	{
+
+	}
+
+
+	public function listClientInvoice()
+	{
+
+	}
+
+	public function generateInvoice()
+	{
+
+	}
+
+	public function removeInvoice()
+	{
+
+	}
+
+
+
+}
