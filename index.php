6434aed7481387e25af780e62b0cfee7754234c7
salmaniz82 <salmaniz.82@gmail.com>
2018-11-15 05:59:49 +0500

    cleared out extra test routes

--- index.php
+++ index.php
@@ -4,23 +4,22 @@ $route = new Route();
 
 
 $route->get('/', function() {
-
 	$data = "Welcome to Haladrive API";
 	view::responseJson($data, 200);
 });
 
 
 
-// JWT AUTHENTICATION CHECKING
+
 
 $route->get('/jwt/check', 'jwtauthCtrl@check');
-// for testing only
+
 $route->post('/jwt/login', 'jwtauthCtrl@login');
 
 $route->post('/jwt/register', 'jwtauthCtrl@register');
 
 
-// when vendor is creating a consumer registration + assignment
+
 $route->post('/jwt/consumer-register', 'jwtauthCtrl@consumerRegister');
 
 $route->get('/jwt/validate', 'jwtauthCtrl@validateToken');
@@ -28,35 +27,12 @@ $route->get('/jwt/validate', 'jwtauthCtrl@validateToken');
 $route->get('/jwt/admin', 'jwtauthCtrl@adminOnlyProtected');
 
 
-// LANGUAGE TESTING
-$route->get('/lang', 'langCtrl@listall');
-
-$route->get('/lang/add','langCtrl@addInterface');
-
-$route->post('/lang/add','langCtrl@save');
-
-$route->post('/lang/debug','langCtrl@debugpost');
-
-$route->put('/lang/{id}', 'langCtrl@update');
-
-$route->put('/lang/test', 'langCtrl@test');
+$route->get('/checkuser', 'testCtrl@checkUserInfo');
 
 
 
-// VALIDATION TESTING
-$route->get('/features','featuresCtrl@index');
-
-$route->post('/features','featuresCtrl@save');
-
-$route->get('/features/{id}','featuresCtrl@single');
-
-$route->put('/features/{id}','featuresCtrl@update');
-
-$route->delete('/features/{id}','featuresCtrl@delete');
 
 
-
-/*CLIENTS API */
 $route->get('/api/clients', 'clientsCtrl@index');
 
 $route->get('/api/clients/civilno', 'clientsCtrl@byCivilNo');
@@ -69,11 +45,11 @@ $route->put('/api/clients/{id}', 'clientsCtrl@update');
 
 $route->delete('/api/clients/{id}',	'clientsCtrl@delete');
 
-// vendor clients
 $route->post('/api/vclients', 'vclientsCtrl@save');
 
 
-/* Finance API */
+
+
 $route->get('/api/finance', 		'financeCtrl@index');
 
 $route->get('/api/finance/{id}', 	'financeCtrl@single');
@@ -86,7 +62,7 @@ $route->delete('/api/finance/{id}',	'financeCtrl@delete');
 
 
 
-/* Insurance API */
+
 $route->get('/api/insurance', 			'insuranceCtrl@index');
 
 $route->get('/api/insurance/{id}', 		'insuranceCtrl@single');
@@ -99,7 +75,7 @@ $route->delete('/api/insurance/{id}',	'insuranceCtrl@delete');
 
 
 
-/* Global Values */
+
 
 $route->get('/api/global', 'globalCtrl@index');
 
@@ -113,7 +89,7 @@ $route->delete('/api/global/{id}', 'globalCtrl@delete');
 
 
 
-/* Global Section */
+
 $route->get('/api/gsection', 'gsectionCtrl@index');
 
 $route->get('/api/gsection/{id}', 'gsectionCtrl@single');
@@ -135,7 +111,7 @@ $route->get('/api/globaljson', 'helperCtrl@loadGlobalJson');
 
 
 
-// brand models
+
 
 $route->get('/api/gbrands', 'brandCtrl@index');
 
@@ -148,7 +124,7 @@ $route->delete('/api/gbrands/{id}', 'brandCtrl@delete');
 $route->put('/api/gbrands/{id}', 'brandCtrl@update');
 
 
-/*vehicles controllers*/
+
 $route->get('/api/vehicles', 'vehiclesCtrl@index');
 
 $route->get('/api/vehicles/q/{params}', 'testCtrl@vehicleQuery');
@@ -171,7 +147,7 @@ $route->put('/api/vehicles/{id}', 'vehiclesCtrl@update');
 $route->delete('/api/vehicles/{id}', 'vehiclesCtrl@delete');
 
 
-/* BOOKINGS CONTROLLERS */
+
 
 $route->get('/api/booking', 'bookingCtrl@index');
 
@@ -186,7 +162,7 @@ $route->post('/api/booking', 'bookingCtrl@commonBookingGateway');
 $route->delete('/api/booking/{id}', 'bookingCtrl@delete');
 
 
-// INVOICE
+
 
 $route->get('/api/invoice', 'invoiceCtrl@index');
 
@@ -194,7 +170,7 @@ $route->delete('/api/invoice/{id}', 'invoiceCtrl@delete');
 
 
 
-// VEHICLE INSURACEN POLICIES
+
 $route->get('/api/vinsurance/{id}', 'vInsuranceCtrl@index');
 
 $route->post('/api/vinsurance', 'vInsuranceCtrl@save');
@@ -204,7 +180,7 @@ $route->put('/api/vinsurance/{id}', 'vInsuranceCtrl@update');
 $route->delete('/api/vinsurance/{id}', 'vInsuranceCtrl@delete');
 
 
-// vehicle finance
+
 
 $route->get('/api/vfinance/{id}', 'vFinanceCtrl@index');
 
@@ -216,11 +192,7 @@ $route->delete('/api/vfinance/{id}', 'vFinanceCtrl@delete');
 
 
 
-// testing route
-$route->get('/checkuser', 'testCtrl@checkUserInfo');
 
-
-// maintenance
 $route->get('/api/maintain/{id}', 'maintenanceCtrl@index');
 
 $route->post('/api/maintain', 'maintenanceCtrl@save');
@@ -232,42 +204,8 @@ $route->delete('/api/maintain/{id}', 'maintenanceCtrl@delete');
 
 
 $route->get('/faker/vehicles/{records}', 'fakerCtrl@vehicleMassInsert');
+
 $route->get('/faker/options/{start_id}/{end_id}', 'fakerCtrl@optionsMassAttachment');
-
-
-$route->get('/multitest', function() {
-
-	$db = new Database();
-	$db->table = 'features';
-
-
-	$features = [];
-
-	$features['cols'] = array('featureEN', 'featureAR', 'status', 'user_id');
-	$features['vals'] = array(
-
-		["Feature EN1", "Feature AR1", 1, 1],
-		["Feature EN2", "Feature AR2", 1, 1]
-		
-	);
-
-
-	try {
-
-		if($db->multiInsert($features))
-		{
-			echo "done";
-		}
-		else {
-			var_dump($db);
-		}
-		
-	} catch (Exception $e) {
-		echo $e->getMessage();
-	}
-
-});
-
 
 $route->get('/moduletest', 'moduleTestCtrl@is_available');
 
